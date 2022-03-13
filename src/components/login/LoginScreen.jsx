32d8ecc7941f652a072533ab890679ed54da4ba5
fernando-ui <fst-96@hotmail.com>
2022-03-13 13:40:40 -0600

    Anadiendo la prop history

--- src/components/login/LoginScreen.jsx
+++ src/components/login/LoginScreen.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history}) => {
+  const handleLogin = () => {
+  history.replace('/');
+  };
+
   return (
-    <h1>LoginScreen</h1>
-  )
-}
+    <div className="container mt-5">
+      <h1>LoginScreen</h1>
+      <hr />
+      <button className="btn btn-warning" onClick={() => handleLogin()}>
+        Ingresar
+      </button>
+    </div>
+  );
+};
